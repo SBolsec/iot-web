4bdede5f1f1ff8f0798a5aaf653e94c1a315cb04
sbolsec <sbolsec@croz.net>
2022-05-25 18:13:38 +0200

    updated api url

--- src/api/paths.tsx
+++ src/api/paths.tsx
@@ -2,8 +2,7 @@ import { FindMessagesQuery } from "./types";
 import { createQuery } from "./util";
 
 const API_BASE_URL =
-  process.env.REACT_API_BASE_URL ||
-  "http://iotrestapplication-env.eba-ye2disus.us-east-1.elasticbeanstalk.com";
+  "http://iotrest-env.eba-d6ximnmy.eu-central-1.elasticbeanstalk.com/";
 
 const api = {
   messages: `${API_BASE_URL}/messages`,
